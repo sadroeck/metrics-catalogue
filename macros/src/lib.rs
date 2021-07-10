fb1aafc577bca6ca3b33fd29c18a96a4d4eb4d51
Sam De Roeck <sadroeck@gmail.com>
2021-07-10 18:22:24 -0400

    [impl] Switch to modular code generation

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,7 +2,8 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use std::borrow::Borrow;
+use std::collections::HashSet;
+use std::sync::Mutex;
 use std::{collections::HashMap, convert::TryFrom, fmt, fmt::Formatter};
 use syn::{
     parse_macro_input, Attribute, Data, DataStruct, DeriveInput, Error, Fields, Ident, Lit, Meta,
@@ -12,20 +13,105 @@ use syn::{
 const HIDDEN_MARKER: &str = "hidden";
 const SKIP_MARKER: &str = "ignore";
 
+lazy_static::lazy_static! {
+    /// Hierarchical mapping of metric scopes
+    static ref METRIC_TREE: Mutex<MetricTree> = {
+        Mutex::new(MetricTree::default())
+    };
+}
+
 #[proc_macro_derive(Metrics, attributes(metric))]
 pub fn derive_metrics(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-
-    MetricTree::default()
-        .parse_struct(input)
-        .unwrap_or_else(|err| err.to_compile_error().into())
+    generate(input).unwrap_or_else(|err| err.to_compile_error().into())
 }
 
-#[derive(Default)]
-struct MetricTree {}
+fn generate(input: DeriveInput) -> Result<TokenStream> {
+    let mut tree = METRIC_TREE.lock().unwrap();
+    tree.parse_struct(input)?;
+    // eprintln!("{:#?}", tree);
+    Ok(if tree.is_complete() {
+        // eprintln!("### COMPLETE - Generating..");
+        tree.generate()
+    } else {
+        quote! {}
+    }
+    .into())
+}
+
+#[derive(Default, Debug)]
+struct MetricTree {
+    scopes: HashMap<String, MetricScope>,
+    required_scopes: HashSet<String>,
+    root_scope: Option<String>,
+}
 
 impl MetricTree {
-    fn parse_struct(&mut self, input: DeriveInput) -> Result<TokenStream> {
+    fn is_complete(&self) -> bool {
+        self.root_scope.is_some()
+            && self
+                .required_scopes
+                .iter()
+                .all(|name| self.scopes.contains_key(name))
+    }
+
+    fn generate_root(&self) -> proc_macro2::TokenStream {
+        let root_name = self.root_scope.clone().unwrap();
+        let root_catalogue = format_ident!("catalogue_{}", root_name);
+        let public_catalogue = quote! {
+            pub mod catalogue {
+                pub use super::#root_catalogue::*;
+            }
+        };
+
+        let root_struct = format_ident!("{}", root_name);
+        let recorder = quote! {
+            impl Recorder for #root_struct {
+                // The following are unused in Stats
+                fn register_counter(&self, _key: &Key, _unit: Option<Unit>, _desc: Option<&'static str>) {}
+
+                fn register_gauge(&self, _key: &Key, _unit: Option<Unit>, _desc: Option<&'static str>) {}
+
+                fn register_histogram(&self, _key: &Key, _unit: Option<Unit>, _desc: Option<&'static str>) {}
+
+                fn record_histogram(&self, _key: &Key, _value: f64) {}
+
+                fn increment_counter(&self, key: &Key, value: u64) {
+                    if let Some(metric) = self.find_counter(key.name()) {
+                        metric.increment(value);
+                    }
+                }
+
+                fn update_gauge(&self, key: &Key, value: GaugeValue) {
+                    if let Some(metric) = self.find_gauge(key.name()) {
+                        match value {
+                            GaugeValue::Increment(val) => metric.increase(val),
+                            GaugeValue::Decrement(val) => metric.decrease(val),
+                            GaugeValue::Absolute(val) => metric.set(val),
+                        }
+                    }
+                }
+            }
+        };
+
+        quote! {
+            #public_catalogue
+
+            #recorder
+        }
+    }
+
+    fn generate(&self) -> proc_macro2::TokenStream {
+        let root = self.generate_root();
+        let scopes = self.scopes.values().map(MetricScope::generate);
+        let combined = std::iter::once(root).chain(scopes);
+
+        quote! {
+            #(#combined)*
+        }
+    }
+
+    fn parse_struct(&mut self, input: DeriveInput) -> Result<()> {
         let struct_data = match &input.data {
             Data::Struct(data) => Struct::from_syn(&input, data),
             Data::Enum(_) | Data::Union(_) => Err(Error::new_spanned(
@@ -33,6 +119,19 @@ impl MetricTree {
                 "Metrics are only supported as structs",
             )),
         }?;
+
+        // TODO: Detection via 'root' attribute
+        let is_root_scope = struct_data.ident == "Test";
+        if is_root_scope && self.root_scope.is_some() {
+            return Err(Error::new_spanned(
+                &input,
+                format!(
+                    "Duplicate root attribute previously detected on {}",
+                    self.root_scope.as_ref().unwrap()
+                ),
+            ));
+        }
+        self.root_scope.get_or_insert(struct_data.ident.to_string());
 
         let mut metrics = vec![];
         let mut other_fields = HashMap::new();
@@ -78,9 +177,9 @@ impl MetricTree {
                             return Err(Error::new_spanned(&input, "Only structs are supported"));
                         };
                         sub_metrics.insert(
-                            orig.ident.as_ref().expect("No identity").clone(),
+                            orig.ident.as_ref().expect("No identity").to_string(),
                             SubMetric {
-                                ident: field_type,
+                                ident: field_type.to_string(),
                                 hidden: field.attributes.hidden,
                             },
                         );
@@ -97,153 +196,25 @@ impl MetricTree {
                     return Err(Error::new_spanned(&input, "Only structs are supported"));
                 };
                 other_fields.insert(
-                    orig.ident.as_ref().expect("No identity").clone(),
-                    field_type,
+                    orig.ident.as_ref().expect("No identity").to_string(),
+                    field_type.to_string(),
                 );
             }
         }
         let scope = MetricScope {
-            name: struct_data.ident.to_string(),
+            struct_name: struct_data.ident.to_string(),
             metrics,
             sub_metrics,
             other_fields,
         };
 
-        let scope_name = format_ident!("{}", scope.name);
-        let inits = scope.initialize_members();
-        let initialize = quote! {
-            impl #scope_name {
-                const fn new() -> Self {
-                    Self {
-                        #(#inits),*
-                    }
-                }
-            }
-        };
+        self.required_scopes.insert(scope.struct_name.clone());
+        self.required_scopes
+            .extend(scope.sub_metrics.iter().map(|(_, m)| &m.ident).cloned());
 
-        let counters = match_metric_names(&scope.metrics, MetricType::Counter);
-        let sub_counters = scope
-            .sub_metrics
-            .iter()
-            .filter(|(_, m)| !m.hidden)
-            .map(|(k, _v)| {
-                quote! { .or_else(|| self.#k.find_counter(name))}
-            });
-        let gauges = match_metric_names(&scope.metrics, MetricType::Gauge);
-        let sub_gauges = scope
-            .sub_metrics
-            .iter()
-            .filter(|(_, m)| !m.hidden)
-            .map(|(k, _v)| {
-                quote! { .or_else(|| self.#k.find_gauge(name))}
-            });
+        self.scopes.insert(struct_data.ident.to_string(), scope);
 
-        let registry_trait = quote! {
-            impl Registry for #scope_name {
-                fn find_counter(&self, name: &str) -> Option<&Counter> {
-                    match name {
-                        #(#counters),*
-                    }
-                    #(#sub_counters)*
-                }
-
-                fn find_gauge(&self, name: &str) -> Option<&Gauge> {
-                    match name {
-                        #(#gauges),*
-                    }
-                    #(#sub_gauges)*
-                }
-            }
-        };
-
-        let metric_keys = scope.metrics.iter().filter(|m| !m.hidden).map(|metric| {
-            let key = format_ident!("{}", metric.key);
-            let name = format_ident!("{}", metric.name).to_string();
-            let kv = quote! { #key: &str = #name };
-            quote! { pub const #kv; }
-        });
-        let sub_metric_spaces =
-            scope
-                .sub_metrics
-                .iter()
-                .filter(|(_, m)| !m.hidden)
-                .map(|(k, v)| {
-                    let internal_mod = format_ident!("catalogue_{}", v.ident);
-                    let public_mod = format_ident!("{}", k);
-                    quote! {
-                        pub mod #public_mod {
-                            #[allow(non_camel_case_types)]
-                            pub use super::super::#internal_mod::*;
-                        }
-                    }
-                });
-        let keys = metric_keys.chain(sub_metric_spaces);
-        let name_mod = format_ident!("catalogue_{}", scope_name);
-        let internal_catalogue = quote! {
-            #[allow(non_camel_case_types)]
-            pub mod #name_mod {
-                #(#keys)*
-            }
-        };
-
-        // TODO: Conditional if this is the root metric
-        let public_catalogue = if scope_name == "Test" {
-            quote! {
-                pub mod catalogue {
-                    pub use super::#name_mod::*;
-                }
-            }
-        } else {
-            quote! {}
-        };
-
-        // TODO: Conditional if this is the root metric
-        let recorder = if scope_name == "Test" {
-            quote! {
-                impl Recorder for #scope_name {
-                    // The following are unused in Stats
-                    fn register_counter(&self, _key: &Key, _unit: Option<Unit>, _desc: Option<&'static str>) {}
-
-                    fn register_gauge(&self, _key: &Key, _unit: Option<Unit>, _desc: Option<&'static str>) {}
-
-                    fn register_histogram(&self, _key: &Key, _unit: Option<Unit>, _desc: Option<&'static str>) {}
-
-                    fn record_histogram(&self, _key: &Key, _value: f64) {}
-
-                    fn increment_counter(&self, key: &Key, value: u64) {
-                        if let Some(metric) = self.find_counter(key.name()) {
-                            metric.increment(value);
-                        }
-                    }
-
-                    fn update_gauge(&self, key: &Key, value: GaugeValue) {
-                        if let Some(metric) = self.find_gauge(key.name()) {
-                            match value {
-                                GaugeValue::Increment(val) => metric.increase(val),
-                                GaugeValue::Decrement(val) => metric.decrease(val),
-                                GaugeValue::Absolute(val) => metric.set(val),
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            quote! {}
-        };
-
-        let combined = quote! {
-            #initialize
-
-            #registry_trait
-
-            #internal_catalogue
-
-            #public_catalogue
-
-            #recorder
-        };
-
-        Ok(combined.into())
+        Ok(())
     }
 }
 
@@ -268,46 +239,138 @@ fn match_metric_names<'a>(
         .chain(std::iter::once(fallthrough))
 }
 
-fn default_init((k, v): (impl Borrow<Ident>, impl Borrow<Ident>)) -> proc_macro2::TokenStream {
-    let k = k.borrow();
-    let v = v.borrow();
+fn default_init((k, v): (impl AsRef<str>, impl AsRef<str>)) -> proc_macro2::TokenStream {
+    let k = format_ident!("{}", k.as_ref());
+    let v = format_ident!("{}", v.as_ref());
     quote! { #k: #v::new() }
 }
 
 #[derive(Debug)]
 struct SubMetric {
-    ident: Ident,
+    ident: String,
     hidden: bool,
 }
 
-impl Borrow<Ident> for &SubMetric {
-    fn borrow(&self) -> &Ident {
+impl AsRef<str> for SubMetric {
+    fn as_ref(&self) -> &str {
         &self.ident
     }
 }
 
 #[derive(Debug)]
 struct MetricScope {
-    name: String,
+    struct_name: String,
     metrics: Vec<MetricInstance>,
-    sub_metrics: HashMap<Ident, SubMetric>,
-    other_fields: HashMap<Ident, Ident>,
+    sub_metrics: HashMap<String, SubMetric>,
+    other_fields: HashMap<String, String>,
 }
 
 impl MetricScope {
-    fn initialize_members<'a>(&'a self) -> impl Iterator<Item = proc_macro2::TokenStream> + 'a {
+    fn generate(&self) -> proc_macro2::TokenStream {
+        let initialize = self.generate_init();
+        let registry_trait = self.generate_registry_trait();
+        let internal_catalogue = self.generate_internal_catalogue();
+
+        quote! {
+            #initialize
+
+            #registry_trait
+
+            #internal_catalogue
+        }
+    }
+
+    fn generate_init(&self) -> proc_macro2::TokenStream {
+        let struct_name = format_ident!("{}", &self.struct_name);
         let metric_inits = self
             .metrics
             .iter()
-            .map(|f| {
-                let ident = format_ident!("{}", f.instance);
-                let default = format_ident!("{}", f.metric_type.to_string());
-                (ident, default)
-            })
+            .map(|f| (f.instance.clone(), f.metric_type.to_string()))
             .map(default_init);
         let other_inits = self.other_fields.iter().map(default_init);
         let sub_metrics = self.sub_metrics.iter().map(default_init);
-        metric_inits.chain(other_inits).chain(sub_metrics)
+        let inits = metric_inits.chain(other_inits).chain(sub_metrics);
+
+        quote! {
+            impl #struct_name {
+                const fn new() -> Self {
+                    Self {
+                        #(#inits),*
+                    }
+                }
+            }
+        }
+    }
+
+    fn generate_registry_trait(&self) -> proc_macro2::TokenStream {
+        let struct_name = format_ident!("{}", &self.struct_name);
+        let counters = match_metric_names(&self.metrics, MetricType::Counter);
+        let sub_counters = self
+            .sub_metrics
+            .iter()
+            .filter(|(_, m)| !m.hidden)
+            .map(|(k, _v)| {
+                let sub = format_ident!("{}", k);
+                quote! { .or_else(|| self.#sub.find_counter(name))}
+            });
+        let gauges = match_metric_names(&self.metrics, MetricType::Gauge);
+        let sub_gauges = self
+            .sub_metrics
+            .iter()
+            .filter(|(_, m)| !m.hidden)
+            .map(|(k, _v)| {
+                let sub = format_ident!("{}", k);
+                quote! { .or_else(|| self.#sub.find_gauge(name))}
+            });
+
+        quote! {
+            impl Registry for #struct_name {
+                fn find_counter(&self, name: &str) -> Option<&Counter> {
+                    match name {
+                        #(#counters),*
+                    }
+                    #(#sub_counters)*
+                }
+
+                fn find_gauge(&self, name: &str) -> Option<&Gauge> {
+                    match name {
+                        #(#gauges),*
+                    }
+                    #(#sub_gauges)*
+                }
+            }
+        }
+    }
+
+    fn generate_internal_catalogue(&self) -> proc_macro2::TokenStream {
+        let metric_keys = self.metrics.iter().filter(|m| !m.hidden).map(|metric| {
+            let key = format_ident!("{}", metric.key);
+            let name = format_ident!("{}", metric.name).to_string();
+            let kv = quote! { #key: &str = #name };
+            quote! { pub const #kv; }
+        });
+        let sub_metric_spaces = self
+            .sub_metrics
+            .iter()
+            .filter(|(_, m)| !m.hidden)
+            .map(|(k, v)| {
+                let internal_mod = format_ident!("catalogue_{}", v.ident);
+                let public_mod = format_ident!("{}", k);
+                quote! {
+                    pub mod #public_mod {
+                        #[allow(non_camel_case_types)]
+                        pub use super::super::#internal_mod::*;
+                    }
+                }
+            });
+        let keys = metric_keys.chain(sub_metric_spaces);
+        let name_mod = format_ident!("catalogue_{}", self.struct_name);
+        quote! {
+            #[allow(non_camel_case_types)]
+            pub mod #name_mod {
+                #(#keys)*
+            }
+        }
     }
 }
 
