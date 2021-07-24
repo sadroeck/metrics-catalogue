7f225dc5debeee7e888e263120acf21f77ea3e6b
Sam De Roeck <sadroeck@gmail.com>
2021-07-24 17:56:31 +0200

    [impl] Add Histogram tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,10 +1,13 @@
-use metrics_catalogue::{Catalogue, Counter, DiscreteGauge, Gauge, Key, Recorder};
+use metrics_catalogue::{
+    Catalogue, Counter, DiscreteGauge, Gauge, Histogram, HistogramMetric, Key, Recorder,
+};
 
 #[derive(Catalogue)]
 #[metric(root)]
 struct Test {
     my_b: Counter,
     my_g: Gauge,
+    my_h_60: Histogram<60>,
     my_discrete_g: DiscreteGauge,
 
     // Name override
@@ -23,12 +26,14 @@ struct Test {
 
     // Fully-qualified types
     my_full_counter: ::metrics_catalogue::Counter,
+    my_h_30: ::metrics_catalogue::Histogram<30>,
 }
 
 #[derive(Catalogue)]
 struct SubTest {
     my_t_a: Gauge,
     my_t_b: Counter,
+    my_t_h: Histogram<5>,
     my_sub_sub: SubSubTest,
 }
 
@@ -43,9 +48,11 @@ fn key_generation() {
     let known_names = [
         (catalogue::MY_B, "my_b"),
         (catalogue::MY_G, "my_g"),
+        (catalogue::MY_H_60, "my_h_60"),
         (catalogue::MY_COUNTER_A, "my_counter_a"),
         (catalogue::my_test::MY_T_A, "my_test.my_t_a"),
         (catalogue::my_test::MY_T_B, "my_test.my_t_b"),
+        (catalogue::my_test::MY_T_H, "my_test.my_t_h"),
         (
             catalogue::my_test::my_sub_sub::MY_S_T_A,
             "my_test.my_sub_sub.my_s_t_a",
@@ -56,6 +63,7 @@ fn key_generation() {
         ),
         (catalogue::my_second_test::MY_T_A, "my_second_test.my_t_a"),
         (catalogue::my_second_test::MY_T_B, "my_second_test.my_t_b"),
+        (catalogue::my_second_test::MY_T_H, "my_second_test.my_t_h"),
         (
             catalogue::my_second_test::my_sub_sub::MY_S_T_A,
             "my_second_test.my_sub_sub.my_s_t_a",
@@ -64,6 +72,8 @@ fn key_generation() {
             catalogue::my_second_test::my_sub_sub::MY_S_T_B,
             "my_second_test.my_sub_sub.my_s_t_b",
         ),
+        (catalogue::MY_FULL_COUNTER, "my_full_counter"),
+        (catalogue::MY_H_30, "my_h_30"),
     ];
     known_names.iter().for_each(|(k, v)| assert_eq!(k, v));
 }
@@ -112,5 +122,33 @@ fn hidden_counters() {
         let pre = field.read();
         t.increment_counter(&Key::from_name(key), 1);
         assert_eq!(field.read(), pre, "key {} updated the counter", key);
+    }
+}
+
+#[test]
+fn histograms() {
+    let t = Test::new();
+    let registered_histograms = [
+        (catalogue::MY_H_60, &t.my_h_60 as &dyn HistogramMetric),
+        (catalogue::MY_H_30, &t.my_h_30 as &dyn HistogramMetric),
+        (
+            catalogue::my_test::MY_T_H,
+            &t.my_test.my_t_h as &dyn HistogramMetric,
+        ),
+        (
+            catalogue::my_second_test::MY_T_H,
+            &t.my_second_test.my_t_h as &dyn HistogramMetric,
+        ),
+    ];
+    for (key, field) in registered_histograms {
+        assert!(field.read().is_empty(), "Histogram was not empty initially");
+        t.record_histogram(&Key::from_name(key), 1.0);
+        let samples = field.read();
+        assert_eq!(
+            samples,
+            vec![1.0f64],
+            "key {} did not update the histogram",
+            key
+        );
     }
 }
